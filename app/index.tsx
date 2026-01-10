e696a078cb42f41f19738351d4b25308cb7f90b8
Grian Gajila <griangajila2@gmail.com>
2026-01-10 15:14:59 +0800

    chore: setup commitlint, cspell, prettier, husky and eslint

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+   return (
+      <View>
+         <Text className="text-2xl text-purple-500">
+            Edit app/index.tsx to edit this screen.
+         </Text>
+      </View>
+   );
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+	expoConfig,
+	{
+		ignores: [
+			"dist/*",
+			"node_modules/*",
+			"ios/*",
+			"android/*",
+			"bin/*",
+			"build/*",
+			"expo-env.d.ts",
+			"nativewind-env.d.ts",
+			"bun.lock",
+			".expo/*",
+		],
+	},
 ]);
